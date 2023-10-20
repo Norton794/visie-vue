291252d95287250b826c66bd7fd835ff0cc2c457
Norton Santos <norton.santos@NAVASLINS.LOCAL>
2023-10-20 16:52:30 -0300

    feat: Header and Footer Components

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+  <footer style="position: fixed; bottom: 0; width: 100%" class="bg-dark text-center text-white">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      © {{ new Date().getFullYear() }}
+      <a class="text-white" href="https://github.com/Norton794/">Norton dos Santos</a>
+    </div>
+  </footer>
+</template>
+
+<style scoped></style>
